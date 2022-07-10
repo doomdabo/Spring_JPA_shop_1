e4503290f01eb8a2118e56ffd96193963ebe5de6
Bokyung Kwon <bbo1209@naver.com>
2022-07-10 22:49:33 +0900

    First

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,6 +3,8 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.lang.model.SourceVersion;
+
 @SpringBootApplication
 public class JpashopApplication {
 
